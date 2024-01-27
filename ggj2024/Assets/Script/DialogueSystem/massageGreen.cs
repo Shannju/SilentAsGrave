1382bfdde6db6f984470dd391f213e7e5ac8c920
DirtyRattie <1010044316@qq.com>
2024-01-28 06:02:45 +0800

    602

--- ggj2024/Assets/Script/DialogueSystem/massageGreen.cs
+++ ggj2024/Assets/Script/DialogueSystem/massageGreen.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class massageGreen : MonoBehaviour
 {
-    
+    public Transform spTrans01; // 生成位置1
+    public Transform spTrans02; // 生成位置2
+    public Transform spTrans03;
+    public Transform spTrans04;
+    public Transform spTrans05;
+    private Transform[] MovePositions;
+    public GameObject WxmassageGreen_Short;
+    public float GenerateInterval = 10.0f;
+
     public int healthMassageGreen = 9;
     private Animator animator;
+    private bool KeyDown = false;
+    public float moveDuration = 1.0f; // 协程中，平滑移动的距离。值越小，移动越快。
     [SerializeField] private Collider2D col;
 
     void Start()
