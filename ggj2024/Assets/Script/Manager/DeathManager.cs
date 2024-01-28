105acb811afc6da4e3d94d7ed5aa3dac5ef8f8dd
DirtyRattie <1010044316@qq.com>
2024-01-28 15:13:11 +0800

    1513

--- ggj2024/Assets/Script/Manager/DeathManager.cs
+++ ggj2024/Assets/Script/Manager/DeathManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DeathManager : MonoBehaviour
 {
@@ -11,9 +12,9 @@ public class DeathManager : MonoBehaviour
     private bool P1isInside = false;
     private bool P2isInside = false;
     private bool P3isInside = false;
-    public Text stayTimeText1; // UI文本元素的引用
-    public Text stayTimeText2;
-    public Text stayTimeText3;
+    public TextMeshProUGUI stayTimeText1; // UI文本元素的引用
+    public TextMeshProUGUI stayTimeText2;
+    public TextMeshProUGUI stayTimeText3;
     [SerializeField]public PlayerController1 player1;
     [SerializeField]public PlayerController2 player2;
     [SerializeField]public PlayerController3 player3;
@@ -55,7 +56,7 @@ public class DeathManager : MonoBehaviour
         if (P1isInside)
         {
             P1stayTime += Time.deltaTime;
-            stayTimeText1.text = P1stayTime.ToString("F2");
+            stayTimeText1.text = "P1:" + P1stayTime.ToString("F2") + "To DIE!!";
             if (P1stayTime >= 3.0f)
             {
                 player1.Die();
@@ -63,12 +64,12 @@ public class DeathManager : MonoBehaviour
         }
         else
         {
-
+            stayTimeText1.text = "";
         }
         if (P2isInside)
         {
             P2stayTime += Time.deltaTime;
-            stayTimeText2.text = P2stayTime.ToString("F2");
+            stayTimeText2.text = "P2:" + P2stayTime.ToString("F2") + "To DIE!!";
             if (P2stayTime >= 3.0f)
             {
                 player2.Die();
@@ -76,12 +77,12 @@ public class DeathManager : MonoBehaviour
         }
         else
         {
-
+            stayTimeText2.text = "";
         }
         if (P3isInside)
         {
             P3stayTime += Time.deltaTime;
-            stayTimeText3.text = P3stayTime.ToString("F2");
+            stayTimeText3.text = "P3:" + P3stayTime.ToString("F2") + "To DIE!!";
             if (P3stayTime >= 3.0f)
             {
                 player3.Die();
@@ -89,7 +90,7 @@ public class DeathManager : MonoBehaviour
         }
         else
         {
-
+            stayTimeText3.text = "";
         }
     }
 }
