0fea85df13f221299f324f5288a3d470b0caf361
DirtyRattie <1010044316@qq.com>
2024-01-28 16:02:14 +0800

    1602

--- ggj2024/Assets/Script/Manager/DeathManager.cs
+++ ggj2024/Assets/Script/Manager/DeathManager.cs
@@ -12,6 +12,10 @@ public class DeathManager : MonoBehaviour
     private bool P1isInside = false;
     private bool P2isInside = false;
     private bool P3isInside = false;
+    private bool temp1 = false;
+    private bool temp2 = false;
+    private bool temp3 = false;
+
     public TextMeshProUGUI stayTimeText1; // UI文本元素的引用
     public TextMeshProUGUI stayTimeText2;
     public TextMeshProUGUI stayTimeText3;
@@ -24,7 +28,7 @@ public class DeathManager : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player1") { // 确保与您要跟踪的对象标签匹配
             P1isInside = true;
         }
@@ -36,18 +40,21 @@ public class DeathManager : MonoBehaviour
         }
     }
 
-    void OnTriggerExit(Collider other) {
+    void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.tag == "Player1") {
             P1isInside = false;
             P1stayTime = 0f;
+            temp1 = false;
         }
         if (other.gameObject.tag == "Player2") {
             P2isInside = false;
             P2stayTime = 0f;
+            temp2 = false;
         }
         if (other.gameObject.tag == "Player3") {
             P3isInside = false;
             P3stayTime = 0f;
+            temp3 = false;
         }
     }
 
@@ -56,10 +63,12 @@ public class DeathManager : MonoBehaviour
         if (P1isInside)
         {
             P1stayTime += Time.deltaTime;
-            stayTimeText1.text = "P1:" + P1stayTime.ToString("F2") + "To DIE!!";
-            if (P1stayTime >= 3.0f)
+            stayTimeText1.text = "P2"+ (3f - P1stayTime).ToString("F2")+ "To DIE!!";
+            if (P1stayTime >= 3.0f && !temp1)
             {
                 player1.Die();
+                stayTimeText1.text = "P1 Dying";
+                temp1 = true;
             }
         }
         else
@@ -69,10 +78,12 @@ public class DeathManager : MonoBehaviour
         if (P2isInside)
         {
             P2stayTime += Time.deltaTime;
-            stayTimeText2.text = "P2:" + P2stayTime.ToString("F2") + "To DIE!!";
-            if (P2stayTime >= 3.0f)
+            stayTimeText2.text = "P2"+ (3f - P2stayTime).ToString("F2")+ "To DIE!!";
+            if (P2stayTime >= 3.0f && !temp2)
             {
                 player2.Die();
+                stayTimeText2.text = "P2 Dying";
+                temp2 = true;
             }
         }
         else
@@ -82,10 +93,12 @@ public class DeathManager : MonoBehaviour
         if (P3isInside)
         {
             P3stayTime += Time.deltaTime;
-            stayTimeText3.text = "P3:" + P3stayTime.ToString("F2") + "To DIE!!";
-            if (P3stayTime >= 3.0f)
+            stayTimeText3.text = "P2"+ (3f - P3stayTime).ToString("F2")+ "To DIE!!";
+            if (P3stayTime >= 3.0f && !temp3)
             {
                 player3.Die();
+                stayTimeText3.text = "P3 Dying";
+                temp3 = true;
             }
         }
         else
